cbd466325db63ace6a4ff7edec6e88dae356602a
Kyle Wang <z82101096@2001-b011-3801-1860-1896-ef71-94ec-4f26.dynamic-ip6.hinet.net>
2019-03-14 00:23:06 +0800

    Initial Commit

---
+++ Titan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Titan
+//
+//  Created by Kyle Wang on 14/03/2019.
+//  Copyright © 2019 Kyle Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
