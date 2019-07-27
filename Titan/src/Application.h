2c3673e5955e5f7442063d29a100ddf24f03ec44
Kyle Wang <z82101096@Kyles-MacBook-Pro-2.local>
2019-07-27 14:18:16 +0800

    Patch last orthographic camera commit

--- Titan/src/Application.h
+++ Titan/src/Application.h
@@ -11,6 +11,7 @@
 #include "Renderer/Shader.h"
 #include "Renderer/Buffer.h"
 #include "Renderer/VertexArray.h"
+#include "Renderer/OrthographicCamera.h"
 
 namespace Titan {
     
@@ -42,6 +43,8 @@ namespace Titan {
 
 		std::shared_ptr<Shader> m_BlueShader;
 		std::shared_ptr<VertexArray> m_SquareVA;
+
+		OrthographicCamera m_Camera;
 
 	private:
 		static Application* s_Instance;
