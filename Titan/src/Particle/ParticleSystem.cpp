9c5bffd6ea2ca7a556eecd0877fa76fb7ba9b791
KyleWang <z82101096@hotmail.com>
2020-02-13 14:58:05 -0800

    Added Particle System and Random class

---
+++ Titan/src/Particle/ParticleSystem.cpp
@@ -0,0 +1,98 @@
+#include "tpch.h"
+#include "ParticleSystem.h"
+#include "Renderer/Renderer2D.h"
+#include "Utility/Random.h"
+#include <glm/gtx/compatibility.hpp>
+
+namespace Titan {
+	
+	ParticleSystem::ParticleSystem()
+	{
+		m_ParticlePool.resize(1000);
+		m_BufferData.ParticleVertexArray = VertexArray::Create();
+	}
+
+	ParticleSystem::~ParticleSystem()
+	{
+	}
+
+	void ParticleSystem::Update(float ts)
+	{
+		for (auto& particle : m_ParticlePool)
+		{
+			if (!particle.Active)
+				continue;
+
+			if (particle.LifeRemaining <= 0.0f)
+			{
+				particle.Active = false;
+				continue;
+			}
+			
+			particle.LifeRemaining -= ts;
+			particle.Position += particle.Velocity * ts;
+			particle.Rotation += 0.01f * ts;
+		}
+	}
+
+	void ParticleSystem::Render()
+	{
+		if (!m_BufferData.ParticleVertexArray)
+			return;
+
+		float vertices[] =
+		{
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 0.0f,	1.0f, 0.0f,
+			 0.5f,  0.5f, 0.0f,	1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f,	0.0f, 1.0f
+		};
+
+		m_BufferData.ParticleVertexBuffer = VertexBuffer::Create(vertices, sizeof(vertices));
+		m_BufferData.ParticleVertexBuffer->SetLayout({
+			{ ShaderDataType::Float3, "a_Position" },
+			{ ShaderDataType::Float2, "a_TexCoord" }
+			});
+		m_BufferData.ParticleVertexArray->AddVertexBuffer(m_BufferData.ParticleVertexBuffer);
+
+		uint32_t quadIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		m_BufferData.ParticleIndexBuffer = IndexBuffer::Create(quadIndices, sizeof(quadIndices) / sizeof(uint32_t));
+		m_BufferData.ParticleVertexArray->SetIndexBuffer(m_BufferData.ParticleIndexBuffer);
+
+		for (auto& particle : m_ParticlePool)
+		{
+			if (!particle.Active)
+				continue;
+
+			float life = particle.LifeRemaining / particle.LifeTime;
+			glm::vec4 color = glm::lerp(particle.ColorBegin, particle.ColorEnd, life);
+			float size = glm::lerp(particle.SizeBegin, particle.SizeEnd, life);
+
+			Renderer2D::DrawQuad(m_BufferData.ParticleVertexArray, m_BufferData.ParticleIndexBuffer, particle.Position, glm::vec2(size), particle.Rotation, color);
+		}
+
+	}
+
+	void ParticleSystem::Emit(const Particle& particleProps)
+	{
+		Particle& particle = m_ParticlePool[m_PoolIndex];
+		particle.Active = true;
+		particle.Position = particleProps.Position;
+		particle.Rotation = Random::Float() * 2.0f * glm::pi<float>();
+
+		
+		particle.Velocity = particleProps.Velocity;
+		particle.Velocity += particleProps.VelocityVariation.x * (Random::Float() - 0.5f);
+		particle.Velocity += particleProps.VelocityVariation.y * (Random::Float() - 0.5f);
+
+		particle.ColorBegin = particleProps.ColorBegin;
+		particle.ColorEnd = particleProps.ColorEnd;
+
+		particle.LifeTime = particleProps.LifeTime;
+		particle.LifeRemaining = particleProps.LifeTime;
+		particle.SizeBegin = particleProps.SizeBegin + particleProps.SizeVariation * (Random::Float() - 0.5f);
+		particle.SizeEnd = particleProps.SizeEnd;
+
+		m_PoolIndex = --m_PoolIndex % m_ParticlePool.size();
+	}
+}
---
+++ Titan/src/Particle/ParticleSystem.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <glm/glm.hpp>
+#include "Renderer/OrthographicCamera.h"
+
+namespace Titan {
+
+	struct Particle
+	{
+		glm::vec3 Position;
+		glm::vec3 Velocity, VelocityVariation;
+		glm::vec4 ColorBegin, ColorEnd;
+		float Rotation = 0.0f;
+		float SizeBegin, SizeEnd, SizeVariation;
+		float LifeTime = 1.0f;
+		float LifeRemaining = 1.0f;
+		bool Active = false;
+	};
+
+	class ParticleSystem
+	{
+	public:
+		ParticleSystem();
+		~ParticleSystem();
+		
+		void Update(float ts);
+		void Render();
+		void Emit(const Particle& particle);
+		
+	private:
+		std::vector<Particle> m_ParticlePool;
+		uint32_t m_PoolIndex = 999;
+		
+		struct BufferData
+		{
+			std::shared_ptr<VertexArray> ParticleVertexArray;
+			std::shared_ptr<VertexBuffer> ParticleVertexBuffer;
+			std::shared_ptr<IndexBuffer> ParticleIndexBuffer;
+			std::shared_ptr<Shader> ParticleShader;
+		};
+		BufferData m_BufferData;
+	};
+}
