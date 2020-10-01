38adb15ee2cc514221aa58fb3fe93aad276e82f6
KyleWang <z82101096@hotmail.com>
2020-09-30 23:14:24 -0700

    Fixed upadate

--- Titan/src/Renderer/FrameBuffer.h
+++ Titan/src/Renderer/FrameBuffer.h
@@ -7,12 +7,14 @@ namespace Titan {
 
 	struct FramebufferDesc
 	{
-		std::string Name;
 		uint32_t Width, Height;
 		uint32_t nrColorAttachment = 1;
-		TextureDesc TexDesc;
+		uint32_t DepthFormat;
+		
 		bool Depth = true;
 		bool SwapChainTarget = false;
+		std::string Name;
+		TextureDesc TexDesc;
 	};
 
 	class Framebuffer
@@ -21,12 +23,15 @@ namespace Titan {
 		virtual ~Framebuffer() = default;
 		virtual void Bind() = 0;
 		virtual void Unbind() = 0;
-		virtual void BlitTo(const Framebuffer& fbo, uint32_t mask) = 0;
+		virtual void BlitTo(Framebuffer* fbo, uint32_t mask) = 0;
 
+		virtual void SetFramebufferID(uint32_t id) = 0;
 		virtual uint32_t GetFramebufferID() const = 0;
 		virtual std::vector<std::shared_ptr<Texture2D>> GetColorAttachments() const = 0;
-		virtual std::shared_ptr<Texture2D> GetColorAttachment(uint32_t index) const = 0;
+		virtual std::shared_ptr<Texture2D> GetColorAttachment(uint32_t index = 0) const = 0;
 		virtual std::shared_ptr<Texture2D> GetDepthAttachment() const = 0;
+		virtual void SetColorAttachment(uint32_t id, uint32_t index) = 0;
+		virtual void SetDepthAttachment(uint32_t id) = 0;
 		virtual const FramebufferDesc& GetFramebufferDesc() const = 0;
 		
 		const std::pair<uint32_t, uint32_t> GetFramebufferSize() const { return std::pair<uint32_t, uint32_t>(GetFramebufferDesc().Width, GetFramebufferDesc().Height); }
